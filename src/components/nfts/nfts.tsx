eadaa62f8031e84fa7a8fe584ba2576cb5858349
trashpirate <nadinaoates@gmail.com>
2024-02-17 20:50:24 -0600

    limit displayed nfts

--- src/components/nfts/nfts.tsx
+++ src/components/nfts/nfts.tsx
@@ -1,14 +1,14 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { useAccount, useContractReads, useNetwork } from "wagmi";
-import { nftABI } from "@/assets/nftABI";
-import { tokenABI } from "@/assets/tokenABI";
+import React, {useEffect, useState} from "react";
+import {useAccount, useContractReads, useNetwork} from "wagmi";
+import {nftABI} from "@/assets/nftABI";
+import {tokenABI} from "@/assets/tokenABI";
 import Image from "next/image";
-import { Alchemy, Network } from "alchemy-sdk";
+import {Alchemy, Network} from "alchemy-sdk";
 import Link from "next/link";
 
-const NFT_CONTRACT = process.env.NEXT_PUBLIC_NFT_CONTRACT as `0x${string}`;
-const TOKEN_CONTRACT = process.env.NEXT_PUBLIC_TOKEN_CONTRACT as `0x${string}`;
+const NFT_CONTRACT = process.env.NEXT_PUBLIC_NFT_CONTRACT as `0x${ string }`;
+const TOKEN_CONTRACT = process.env.NEXT_PUBLIC_TOKEN_CONTRACT as `0x${ string }`;
 
 const config = {
   apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
@@ -28,7 +28,7 @@ interface NFTMeta {
 
 type Props = {};
 
-export default function Nfts({ }: Props) {
+export default function Nfts({}: Props) {
   const [maxPerWallet, setMaxPerWallet] = useState<number | undefined>(
     undefined,
   );
@@ -37,10 +37,10 @@ export default function Nfts({ }: Props) {
   const [totalSupply, setTotalSupply] = useState<number | undefined>(undefined);
 
   // get account address
-  const { address, isConnecting, isDisconnected, isConnected } = useAccount({});
+  const {address, isConnecting, isDisconnected, isConnected} = useAccount({});
 
   // get chain
-  const { chain } = useNetwork();
+  const {chain} = useNetwork();
 
   // define token contract config
   const tokenContract = {
@@ -56,7 +56,7 @@ export default function Nfts({ }: Props) {
     chainId: chain?.id,
   };
 
-  const { data, isSuccess, refetch } = useContractReads({
+  const {data, isSuccess, refetch} = useContractReads({
     contracts: [
       {
         ...nftContract,
@@ -65,7 +65,7 @@ export default function Nfts({ }: Props) {
       {
         ...nftContract,
         functionName: "balanceOf",
-        args: [address as `0x${string}`],
+        args: [address as `0x${ string }`],
       },
       {
         ...nftContract,
@@ -98,22 +98,22 @@ export default function Nfts({ }: Props) {
     const nfts = await alchemy.nft.getNftsForOwner(owner, options);
     console.log(nfts);
     let nftArray: NFTMeta[] = [];
-    const maxShow = maxPerWallet ? maxPerWallet : 10;
+    const maxShow = maxPerWallet != undefined && maxPerWallet <= 10 ? maxPerWallet : 10;
     for (let index = 1; index <= maxShow; index++) {
       const nft = nfts["ownedNfts"].at(-index);
       if (nft != undefined) {
         let imageURL: string = "/unrevealed_2.jpg";
 
         const res = await fetch(
-          `https://bafybeic2a7jdsztni6jsnq2oarb3o5g7iuya5r4lcjfqi64rsucirdfobm.ipfs.nftstorage.link/${nft.tokenId}`,
+          `https://bafybeic2a7jdsztni6jsnq2oarb3o5g7iuya5r4lcjfqi64rsucirdfobm.ipfs.nftstorage.link/${ nft.tokenId }`,
         );
         const json = await res.json();
         const [prefix, separator, url, color, name] = json.image.split("/");
-        imageURL = `https://bafybeiaf6ppnztlf3k5edqrgq3zae5ih2y6vhf255hekkqn6vjwazhq36q.ipfs.nftstorage.link/${color}/${name}`;
+        imageURL = `https://bafybeiaf6ppnztlf3k5edqrgq3zae5ih2y6vhf255hekkqn6vjwazhq36q.ipfs.nftstorage.link/${ color }/${ name }`;
         console.log(nft);
         let iNft: NFTMeta = {
           name: nft["contract"].name
-            ? `${nft["contract"].name}  #${nft.tokenId}`
+            ? `${ nft["contract"].name }  #${ nft.tokenId }`
             : "Flameling #?",
           id: Number(nft.tokenId),
           path: imageURL,
@@ -142,7 +142,7 @@ export default function Nfts({ }: Props) {
     <div className="h-full w-full pb-8">
       <div className="mx-auto h-full max-w-sm rounded-md bg-black p-8 sm:w-full md:max-w-none">
         <h2 className="border-b-2 border-primary pb-2 text-justify text-xl uppercase">
-          {`Your NFTs (Max. ${maxPerWallet})`}
+          {`Your recently minted NFTs (Max. ${ maxPerWallet })`}
         </h2>
         <div className="my-4 min-h-max">
           <div className="grid grid-cols-2 place-content-center gap-4 sm:grid-cols-3 md:grid-cols-5 ">
@@ -153,7 +153,7 @@ export default function Nfts({ }: Props) {
                 return (
                   <Link
                     key={nft.id}
-                    href={`https://opensea.io/assets/ethereum/${NFT_CONTRACT}/${nft.id}`}
+                    href={`https://opensea.io/assets/ethereum/${ NFT_CONTRACT }/${ nft.id }`}
                   >
                     <div
                       className={
@@ -164,7 +164,7 @@ export default function Nfts({ }: Props) {
                       {
                         <Image
                           alt={nft.name || ""}
-                          src={`${nft.path}` as string}
+                          src={`${ nft.path }` as string}
                           width={100}
                           height={100}
                           style={{
